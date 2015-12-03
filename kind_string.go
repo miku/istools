efdb3c09a7709a12568721c02281e95d2dac5ced
Martin Czygan <martin.czygan@gmail.com>
2015-12-03 16:50:04 +0100

    update generated code

--- kind_string.go
+++ kind_string.go
@@ -6,11 +6,16 @@ import "fmt"
 
 const _Kind_name = "KeyTooLongInvalidStartPageInvalidEndPageEndPageBeforeStartPageInvalidURLSuspiciousPageCountPublicationDateTooEarlyPublicationDateTooLateInvalidCollectionRepeatedSubtitleCurrencyInTitleExcessivePunctuation"
 
-var _Kind_index = [...]uint8{0, 10, 26, 40, 62, 72, 91, 114, 136, 153, 169, 184, 204}
+var _Kind_index = [...]uint8{10, 26, 40, 62, 72, 91, 114, 136, 153, 169, 184, 204}
 
 func (i Kind) String() string {
-	if i < 0 || i+1 >= Kind(len(_Kind_index)) {
+	if i < 0 || i >= Kind(len(_Kind_index)) {
 		return fmt.Sprintf("Kind(%d)", i)
 	}
-	return _Kind_name[_Kind_index[i]:_Kind_index[i+1]]
+	hi := _Kind_index[i]
+	lo := uint8(0)
+	if i > 0 {
+		lo = _Kind_index[i-1]
+	}
+	return _Kind_name[lo:hi]
 }
