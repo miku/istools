1605e6506c3043aff0f543a4edf17a7c4056674b
Martin Czygan <martin.czygan@gmail.com>
2015-12-07 18:18:51 +0100

    run tests in parallel and output JSON

--- cmd/islint/main.go
+++ cmd/islint/main.go
@@ -8,7 +8,10 @@ import (
 	"io"
 	"log"
 	"os"
-	"os/signal"
+	"runtime"
+	"strconv"
+	"sync"
+	"time"
 
 	"github.com/miku/islint"
 	"github.com/miku/span/finc"
@@ -16,8 +19,106 @@ import (
 
 var (
 	tests   = islint.DefaultTests
+	Verbose = false
 	version = "0.1.3"
+	start   = time.Now()
 )
+
+func worker(queue chan [][]byte, out chan []islint.Issue, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for batch := range queue {
+		for _, b := range batch {
+			var is finc.IntermediateSchema
+			if err := json.Unmarshal(b, &is); err != nil {
+				log.Fatal(err)
+			}
+			var issues []islint.Issue
+			for _, t := range tests {
+				err := t.TestRecord(is)
+				if err != nil {
+					if _, ok := err.(islint.Issue); !ok {
+						log.Fatalf("invalid error type: %T", err)
+					}
+					issues = append(issues, err.(islint.Issue))
+				}
+			}
+			out <- issues
+		}
+	}
+}
+
+type KindFrequencyMap map[islint.Kind]int
+
+func (m KindFrequencyMap) MarshalJSON() ([]byte, error) {
+	sm := make(map[string]int)
+	for k, v := range m {
+		sm[k.String()] = v
+	}
+	return json.Marshal(sm)
+}
+
+// Stats keeps basic stats on issues.
+type Stats struct {
+	IssueDistribution map[islint.Kind]int `json:"issues"`
+	IssuesPerRecord   map[int]int         `json:"frequency"`
+}
+
+// MarshalJSON calculates a few extra metrics on the fly.
+func (s Stats) MarshalJSON() ([]byte, error) {
+	var total, damaged int
+	errcount := make(map[string]int)
+
+	for k, v := range s.IssuesPerRecord {
+		total += v
+		if k > 0 {
+			damaged += v
+		}
+		errcount[strconv.Itoa(k)] = v
+	}
+	ratio := (100 / float64(total)) * float64(damaged)
+
+	dist := make(map[string]int)
+	for k, v := range s.IssueDistribution {
+		dist[k.String()] = v
+	}
+
+	return json.Marshal(map[string]interface{}{
+		"dist":     dist,
+		"errcount": errcount,
+		"total":    total,
+		"damaged":  damaged,
+		"ratio":    fmt.Sprintf("%0.3f", ratio),
+		"elapsed":  time.Since(start).Seconds(),
+	})
+}
+
+func writer(in chan []islint.Issue, done chan bool) {
+	stats := Stats{IssueDistribution: make(map[islint.Kind]int), IssuesPerRecord: make(map[int]int)}
+	var i int
+	for issues := range in {
+		stats.IssuesPerRecord[len(issues)]++
+		for _, issue := range issues {
+			stats.IssueDistribution[issue.Kind]++
+			if Verbose {
+				fmt.Println(issue.TSV())
+			}
+		}
+		i++
+		if i%1000000 == 0 {
+			b, err := json.Marshal(stats)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(string(b))
+		}
+	}
+	b, err := json.Marshal(stats)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(b))
+	done <- true
+}
 
 func main() {
 	verbose := flag.Bool("verbose", false, "show every error")
@@ -30,6 +131,8 @@ func main() {
 		fmt.Println(version)
 		os.Exit(0)
 	}
+
+	Verbose = *verbose
 
 	if *listTests {
 		fmt.Println(`CurrencyInTitle
@@ -53,38 +156,36 @@ WhitespaceAuthor`)
 		os.Exit(0)
 	}
 
-	var rdr io.Reader
+	var r io.Reader
 
 	if flag.NArg() == 0 {
-		rdr = os.Stdin
+		r = os.Stdin
 	} else {
 		file, err := os.Open(flag.Arg(0))
 		if err != nil {
 			log.Fatal(err)
 		}
-		rdr = file
+		r = file
 	}
 
-	reader := bufio.NewReader(rdr)
+	reader := bufio.NewReader(r)
 
-	var i, issues int
+	var i int
+	var batch [][]byte
+	var size = 40000
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
+	queue := make(chan [][]byte)
+	out := make(chan []islint.Issue)
+	done := make(chan bool)
 
-	dist := make(map[islint.Kind]int)
+	var wg sync.WaitGroup
 
-	printSummary := func() {
-		log.Printf("%v total, %v ok, %v or %0.3f%% with issues", i, i-issues, issues, float64(issues)/float64(i-issues)*100)
-		log.Println(dist)
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go worker(queue, out, &wg)
 	}
 
-	// handle signal
-	go func() {
-		<-c
-		printSummary()
-		os.Exit(0)
-	}()
+	go writer(out, done)
 
 	for {
 		b, err := reader.ReadBytes('\n')
@@ -94,35 +195,24 @@ WhitespaceAuthor`)
 		if err != nil {
 			log.Fatal(err)
 		}
+		if i == size {
+			ba := make([][]byte, len(batch))
+			copy(ba, batch)
+			queue <- ba
+			batch = batch[:0]
+			i = 0
+		}
+		batch = append(batch, b)
 		i++
-		var is finc.IntermediateSchema
-		err = json.Unmarshal(b, &is)
-		if err != nil {
-			log.Fatal(err)
-		}
-		hasIssues := false
-		for _, t := range tests {
-			err := t.TestRecord(is)
-			if err != nil {
-				hasIssues = true
-				switch e := err.(type) {
-				case islint.Issue:
-					if *verbose {
-						fmt.Println(e.TSV())
-					}
-					dist[e.Kind]++
-				default:
-					log.Fatalf("invalid error type: %T", err)
-				}
-			}
-		}
-		if hasIssues {
-			issues++
-		}
-		if i%1000000 == 0 {
-			log.Println(i)
-			printSummary()
-		}
 	}
-	printSummary()
+
+	ba := make([][]byte, len(batch))
+	copy(ba, batch)
+	queue <- ba
+	batch = batch[:0]
+
+	close(queue)
+	wg.Wait()
+	close(out)
+	<-done
 }
