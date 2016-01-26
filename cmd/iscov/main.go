3a0096df5a449f36bbccb6318add053afde289e8
Martin Czygan <martin.czygan@gmail.com>
2016-01-26 18:40:24 +0100

    iscov stub

--- cmd/iscov/main.go
+++ cmd/iscov/main.go
@@ -1,7 +1,98 @@
 package main
 
-import "log"
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"io"
+	"log"
+	"os"
+
+	"github.com/miku/holdings"
+	"github.com/miku/holdings/google"
+	"github.com/miku/holdings/kbart"
+	"github.com/miku/holdings/ovid"
+	"github.com/miku/span/finc"
+)
 
 func main() {
-	log.Println("determine holding file coverage of a intermediate schema file")
+	filename := flag.String("file", "", "path to holdings file")
+	format := flag.String("format", "kbart", "holding file format, kbart, google, ovid")
+
+	flag.Parse()
+
+	if *filename == "" {
+		log.Fatal("holding -file required")
+	}
+
+	var r *bufio.Reader
+	if flag.NArg() == 0 {
+		r = bufio.NewReader(os.Stdin)
+	} else {
+		file, err := os.Open(flag.Arg(0))
+		if err != nil {
+			log.Fatal(err)
+		}
+		r = bufio.NewReader(file)
+	}
+
+	hfile, err := os.Open(*filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var hr holdings.File
+
+	switch *format {
+	case "kbart":
+		hr = kbart.NewReader(hfile)
+	case "ovid":
+		hr = ovid.NewReader(hfile)
+	case "google":
+		hr = google.NewReader(hfile)
+	default:
+		log.Fatalf("invalid holding file format: %s", *format)
+	}
+
+	entries, err := hr.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		b, err := r.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		var is finc.IntermediateSchema
+		if err := json.Unmarshal(b, &is); err != nil {
+			log.Fatal(err)
+		}
+		signature := holdings.Signature{
+			Date:   is.Date.Format("2006-01-02"),
+			Volume: is.Volume,
+			Issue:  is.Issue,
+		}
+
+		// validate record, if at least one license allows this item
+		var valid bool
+
+	LOOP:
+		for _, issn := range append(is.ISSN, is.EISSN...) {
+			licenses := entries.Licenses(issn)
+
+			for _, license := range licenses {
+				coverage := license.Covers(signature)
+				wall := license.TimeRestricted(is.Date)
+				if coverage == nil && wall == nil {
+					valid = true
+					break LOOP
+				}
+			}
+		}
+		log.Printf("%s\t%v", is.RecordID, valid)
+	}
 }
